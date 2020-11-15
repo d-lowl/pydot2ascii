8a27e18bd0985a61d4c151defc7c295cf675c361
D. Lowl <dlowl@sihvi.com>
2020-11-15 16:31:12 +0500

    Add layered graph grid-based layouts

--- pydot2ascii/to_ascii.py
+++ pydot2ascii/to_ascii.py
@@ -1,8 +1,10 @@
 from pydot import Dot, graph_from_dot_data
+from .layout import get_graph_layout
 
 def from_dot_string(string: str) -> str:
     # Assuming there's only one graph/digraph in the string
     return from_dot_graph(graph_from_dot_data(string)[0])
 
 def from_dot_graph(graph: Dot) -> str:
+    grid = get_graph_layout(graph)
     return ""
